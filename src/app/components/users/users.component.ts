1d5c52f5a6f179535565507ea2a125451baa1034
Csaba Kiss <cocmangamal@gmail.com>
2019-10-27 12:31:54 +0100

    Initial commit

---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+
+import { User } from '../../models/User';
+
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  users: User[];
+  showExtended: boolean = true;
+  isLoaded: boolean = true;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.users = [
+      {
+        firstName: 'Jhon',
+        lastName: 'Doe',
+        age: 33,
+        address: {
+          street: 'utca',
+          city: 'Budapest',
+          state: 'Budapest'
+        }
+      }, {
+        firstName: 'Gulash',
+        lastName: 'Suppe',
+        age: 24,
+        address: {
+          street: 'Nagy Janos',
+          city: 'Debrecen',
+          state: 'Koros'
+        }
+      }, {
+        firstName: 'Elek',
+        lastName: 'Peterfy',
+        age: 46,
+        address: {
+          street: 'utca',
+          city: 'Szentendre',
+          state: 'Pest'
+        }
+      }
+    ]
+
+    this.showExtended = false;
+  }
+
+  addUser(user: User) {
+    // stuff
+    this.users.push(user);
+  }
+
+}
