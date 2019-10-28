fc4c8189f5b2f21202af14fd1182faa481bcfb33
Csaba Kiss <cocmangamal@gmail.com>
2019-10-28 15:18:44 +0100

    Some cleanup

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -13,12 +13,7 @@ export class UsersComponent implements OnInit {
   newUser: User = {
     firstName: '',
     lastName: '',
-    age: null,
-    address: {
-      street: '',
-      city: '',
-      state: ''
-    }
+    email: ''
   };
   users: User[];
   showExtended: boolean = false;
@@ -33,34 +28,19 @@ export class UsersComponent implements OnInit {
       {
         firstName: 'Jhon',
         lastName: 'Doe',
-        age: 33,
-        address: {
-          street: 'utca',
-          city: 'Budapest',
-          state: 'Budapest'
-        },
+        email: 'Jhon@gmail.com',
         registered: new Date('03/11/2019 10:09:09'),
         isHidden: true
       }, {
         firstName: 'Gulash',
         lastName: 'Suppe',
-        age: 24,
-        address: {
-          street: 'Nagy Janos',
-          city: 'Debrecen',
-          state: 'Koros'
-        },
+        email: 'gulash@gfreemail.com',
         registered: new Date('11/08/2019 12:11:09'),
         isHidden: true
       }, {
         firstName: 'Elek',
         lastName: 'Peterfy',
-        age: 46,
-        address: {
-          street: 'utca',
-          city: 'Szentendre',
-          state: 'Pest'
-        },
+        email: 'elek@gelekfi.com',
         isActive: true,
         registered: new Date('11/08/2016 08:11:09'),
         isHidden: true
@@ -78,12 +58,7 @@ export class UsersComponent implements OnInit {
     this.newUser = {
       firstName: '',
       lastName: '',
-      age: null,
-      address: {
-        street: '',
-        city: '',
-        state: ''
-      }
+      email: ''
     };
   }
 
