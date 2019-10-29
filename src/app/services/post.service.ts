a4ebeb2e5dc56c039f3412c9619ad97c273a8197
Csaba Kiss <cocmangamal@gmail.com>
2019-10-29 19:30:53 +0100

    Posting posts

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -3,6 +3,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Post } from '../models/Post.model';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Contect-Type': 'application/json' })
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -15,5 +19,9 @@ export class PostService {
 
   getPosts(): Observable<Post[]> {
     return this.http.get<Post[]>(this.url);
+  }
+
+  savePost(post: Post): Observable<Post> {
+    return this.http.post<Post>(this.url, post, httpOptions);
   }
 }
