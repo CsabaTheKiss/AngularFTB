3c0efb5a95ff3950c17af58f3d2907f2bd189da7
Csaba Kiss <cocmangamal@gmail.com>
2019-10-29 19:11:59 +0100

    Added http service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/Forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { UsersService } from './services/users.service';
+import { PostService } from './services/post.service';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { UsersComponent } from './components/users/users.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PostsComponent } from './components/posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     UsersComponent,
-    NavbarComponent
+    NavbarComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
-    UsersService
+    UsersService,
+    PostService
   ],
   bootstrap: [AppComponent]
 })
